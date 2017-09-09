205b0c191615abc1638345d2ff2fb6e6288436c3
Mera44 <Mera@meras-MacBook-Pro.local>
2017-09-08 20:37:52 -0500

    lab 4 and lab 5

--- Lab4/Exercise1.js
+++ Lab4/Exercise1.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
 const http = require('http').createServer();
 const url = require('url');
+const {fork} = require('child_process');
 //console.log(url.parse('http://localhost:8000/?url=file.txt',true).query.url);
 
 
 http.on('request',(req,res)=>{
-    //console.log(req.url);
-    const myurl = url.parse(req.url,true);
-    //console.log('printed out===>'+myurl.query.url);
-    let file = myurl.query.url;
-    const readable = fs.createReadStream('./'+file);
-    readable.pipe(res);
     
-
-}).listen(8000);
+    // const myurl = url.parse(req.url,true);
+    // let file = myurl.query.url;
+    // const readable = fs.createReadStream('./'+file);
+    // readable.pipe(res);
+//////////////////////////////////////////////////////////////
+    //Using cloning
+    const myurl = url.parse(req.url,true);
+    let file = myurl.query.url;
+    console.log('===>yes');
+    const childprocess = fork('readfile.js');
+    
+    childprocess.send(file);
+    
+    childprocess.on('message',(msg) =>{
+        //console.log());
+        
+        //console.log(msg);
+        
+    });
+    
+}).listen(5000);
---
+++ Lab4/readfile.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const url = require('url');
+
+var readfile = (file)=>{
+// const myurl = url.parse(link.url,true);
+// let file = myurl.query.url;
+const readable = fs.createReadStream('./'+file);
+console.log('readable '+readable);
+return readable;
+};
+
+process.on('message', msg =>{
+    console.log('from===>child'+msg);
+    const result = readfile(msg);
+
+    process.send(result.toString());
+});
