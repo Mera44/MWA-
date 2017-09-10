d65d3d1de0b94db9f68ffc7ccddc5e9c6a5d4ca2
Mera44 <Mera@meras-MacBook-Pro.local>
2017-09-10 17:30:05 -0500

    Lab 6 finished

---
+++ Lab6/app/routes/login.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'News Letter' });
+});
+router.post('/', function(req, res, next) {
+    req.assert('email','A valid email is required').notEmpty().isEmail();
+    var error = req.validationErrors();
+    //console.log('vR====>'+error.toString());
+    if(error){
+       return res.render('error', { error:error, message:'Wrong Email' });
+    }
+    fs.appendFile('public/subscribers.txt', "  "+req.body.email , function (err) {
+        if (err) {
+         return  next(err);
+        } else {
+            res.render('thankyou', { title: req.body.email });
+        }
+      });
+   
+  });
+module.exports = router;
