1a71750ac4b035d0b4226201b6bd64ff9b0d3de7
Mera44 <Mera@meras-mbp.cs.mum.edu>
2017-09-13 18:53:14 -0500

    Lab 8 and 9 last commit

--- Lab8/myapp/routes/search.js
+++ Lab8/myapp/routes/search.js
@@ -8,16 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('search',{title:'Finder'});
 });
 router.post('/', function(req, res, next) {
-  //console.log('Posted--->'+req.body.long);
   req.mongo.connect(path,function(err,db){
     if(err) throw err;
     var query ={$and:[{category:req.body.category},{location:{$near:[req.body.long,req.body.lat]}}]};
 
   db.collection('service').find(query).toArray(function(err, result) {
     if (err) throw err;
-    //console.dir(result);
+    console.dir(result);
     db.close();
-    res.render('index',{title:"Finder",service:result});
+    res.render('index',{title:'Finder', service:'Updated'})
     });
   })});
 
